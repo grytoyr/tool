221105b537e0a4e62fe58ab76f6aaaf2445d7ab7
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-18 10:59:52 +0100

    new and create method + new view  done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,18 +8,17 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.user = current.user
-    if @cocktail.save
-      redirect_to @cocktail
+    @item.user = current_user
+    if @item.save
+      redirect_to @item
     else
       render :new
     end
+  end
 
-    private
+  private
 
-    def items_params
-      params.require(:item).permit(:name, :category, :price, :description)
-
-       params.require(:cocktail).permit(:name, :image, :doses_id, :ingredients_id, :photo)
-    end
+  def item_params
+    params.require(:item).permit(:name, :category, :price, :description)
+  end
 end
