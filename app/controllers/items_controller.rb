234a2d183513a4e034843f6733060b8d7011c55d
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-18 10:48:38 +0100

    working on create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,21 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current.user
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+
+    private
+
+    def items_params
+      params.require(:item).permit(:name, :category, :price, :description)
+
+       params.require(:cocktail).permit(:name, :image, :doses_id, :ingredients_id, :photo)
+    end
 end
