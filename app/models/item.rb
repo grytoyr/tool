b37fe7ed4e3ea03226b7bf6e1f97a2fc4dfd52db
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-02-19 12:34:42 +0100

    index page, list of items and map implemented

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   has_many :rentals
   has_one_attached :photo
   validates :photo, attached: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
